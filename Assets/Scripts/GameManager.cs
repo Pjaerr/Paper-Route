96af43356336eaec94f222b359895887461b1d9b
Joshua Jackson <washingtonfaulconer@gmail.com>
2018-01-26 19:51:50 +0000

    Initial project commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static public GameManager singleton;
+
+    void Awake()
+    {
+        if (singleton == null)
+        {
+            singleton = this;
+        }
+        else if (singleton != this)
+        {
+            Destroy(this);
+        }
+    }
+}
